204de2aeb6b6b86587640ca4e45a8fc776b6b6b7
RAKSO44 <oscarcruzaleguicastillo@gmail.com>
2025-10-14 01:20:58 -0500

    Adding Local Storage, Clear Button, Send time

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Chatbot } from 'supersimpledev'
 import { ChatInput } from './components/ChatInput'
 import ChatMessages from './components/ChatMessages';
 
@@ -6,30 +7,41 @@ import './App.css'
 
 
 function App(){
-      
-      const [chatMessages, setChatMessages] = useState([]);
-      
-      // const [chatMessages, setChatmessages] = array;
-      // const chatMessages = array[0];
-      // const setChatMessages = array[1];
 
-      return(
-        <div className="app-container">
-          {chatMessages.length === 0 
-            && (
-              <p className="welcome-message">
-                Welcome to the chatbot project! Send a message using the textbox below.
-              </p>
-            )}
-          <ChatMessages 
-            chatMessages={chatMessages}
-          />
-          <ChatInput 
-            chatMessages={chatMessages}
-            setChatMessages={setChatMessages}
-          />     
-        </div>       
-      );
-    }
+  useEffect(() => {
+    Chatbot.addResponses({
+      'nigga': `yo I didn't know u were chill like that`,
+      'give me a unique id':`Sure! Here's a unique ID: ${crypto.randomUUID()}`    
+    })
+  }, []);
+  
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || []);
+  
+  // const [chatMessages, setChatmessages] = array;
+  // const chatMessages = array[0];
+  // const setChatMessages = array[1];
+
+  useEffect(() => {
+    localStorage.setItem('messages', JSON.stringify(chatMessages));
+  }, [chatMessages]);
+
+  return(
+    <div className="app-container">
+      {chatMessages.length === 0 
+        && (
+          <p className="welcome-message">
+            Welcome to the chatbot project! Send a message using the textbox below.
+          </p>
+        )}
+      <ChatMessages 
+        chatMessages={chatMessages}
+      />
+      <ChatInput 
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />     
+    </div>       
+  );
+}
 
 export default App
--- chatbot-project/src/components/ChatMessage.jsx
+++ chatbot-project/src/components/ChatMessage.jsx
@@ -1,42 +1,49 @@
+import dayjs from 'dayjs';
 import './ChatMessage.css';
 import RobotProfileImage from '../assets/robot.png'
 import UserProfileImage from '../assets/user.png'
+import MyProfileImage from '../assets/me.jpg'
 
-export function ChatMessage({ message, sender }){ //destructuring 
-      // const message = props.message;
-      // const sender = props.sender;
-      // const { message, sender } = props;  
-      
-      /*
-      if(sender === 'robot'){
-        return(
-          <div>
-            <img src="robot.png" width="50"/>
-            {message}
-          </div>
-        );
-      }
-      */
+export function ChatMessage({ message, sender, time }){ //destructuring 
 
+  // const message = props.message;
+  // const sender = props.sender;
+  // const { message, sender } = props;  
+  /*
+  if(sender === 'robot'){
     return(
-      <div className={
-        sender === 'user' 
-          ? 'chat-message-user' 
-          : 'chat-message-robot'
-      }>
-        {sender === 'robot' && (
-          <img src={RobotProfileImage}
-          className="chat-message-profile"
-          />
-        )}
-        <div className="chat-message-text">
-          {message} 
-        </div>
-        {sender === 'user' && (
-          <img src={UserProfileImage}
-          className="chat-message-profile"
-          />
-        )}
+      <div>
+        <img src="robot.png" width="50"/>
+        {message}
       </div>
     );
-    }
+  }
+  */
+
+  return(
+    <div className={
+      sender === 'user' 
+        ? 'chat-message-user' 
+        : 'chat-message-robot'
+    }>
+      {sender === 'robot' && (
+        <img src={RobotProfileImage}
+        className="chat-message-profile"
+        />
+      )}
+      <div className="chat-message-text">
+        {message}
+        {time && (
+          <div className='chat-message-time'>
+            {dayjs(time).format('h:mma')}
+          </div>
+        )}
+      </div>
+      {sender === 'user' && (
+        <img src={MyProfileImage}
+        className="chat-message-profile"
+        />
+      )}
+    </div>
+  );
+}
