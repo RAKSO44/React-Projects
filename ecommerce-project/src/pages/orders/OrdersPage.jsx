4a979ce53428cd8f4163d5a07992647b5ae0d9ee
RAKSO44 <oscarcruzaleguicastillo@gmail.com>
2025-10-17 19:25:59 -0500

    Separate into smaller componentes

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import { useState, useEffect, Fragment } from 'react';
 import { Link } from 'react-router';
-import { Header } from '../components/Header';
-import { formatMoney } from '../utils/money';
+import { Header } from '../../components/Header';
+import { formatMoney } from '../../utils/money';
 import './OrdersPage.css';
-import BuyAgainIcon from '../assets/images/icons/buy-again.png';
+import BuyAgainIcon from '../../assets/images/icons/buy-again.png';
 
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
