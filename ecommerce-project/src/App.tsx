5609b74f0d0bb1a9111189428d54130eac2b6bae
RAKSO44 <oscarcruzaleguicastillo@gmail.com>
2025-11-03 15:16:33 -0500

    Convert ecommerce project to TypeScript

---
+++ ecommerce-project/src/App.tsx
@@ -0,0 +1,42 @@
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/home/HomePage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// window.axios = axios;
+// In console:
+// axios.post('/api/reset');
+// To reset the backend into the default version
+
+function App() {
+
+  const [cart, setCart] = useState([]);
+
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product')
+    setCart(response.data);
+  }
+
+  useEffect(() => {
+    loadCart();
+  }, []);
+
+
+  return (
+    <Routes>
+      <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} loadCart={loadCart} />} />
+      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart} />} />
+      <Route path="*" element={<NotFoundPage cart={cart} />} />
+    </Routes>
+
+  )
+}
+
+export default App
---
+++ old-projects/ecommerce-project-js/src/components/Header.jsx
@@ -0,0 +1,67 @@
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
+import { useState } from 'react';
+import CartIcon from '../assets/images/icons/cart-icon.png';
+import SearchIcon from '../assets/images/icons/search-icon.png';
+import LogoWhite from '../assets/images/logo-white.png';
+import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
+import './Header.css';
+
+
+export function Header({ cart }) {
+
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const searchText = searchParams.get('search');
+  const [search, setSearch] = useState(searchText || '');
+
+  let cartQuantity = 0;
+  
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity
+  });
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchProduct = () => {
+    navigate(`/?search=${search}`);
+  }
+
+return (
+    <div className="header">
+      <div className="left-section">
+        <NavLink to="/" className="header-link">
+          <img className="logo"
+            src={LogoWhite} />
+          <img className="mobile-logo"
+            src={MobileLogoWhite} />
+        </NavLink>
+      </div>
+
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" 
+        value={search} onChange={updateSearchInput} />
+
+        <button className="search-button">
+          <img className="search-icon" src={SearchIcon} 
+           onClick={searchProduct} />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <NavLink className="orders-link header-link" to="/orders">
+
+          <span className="orders-text">Orders</span>
+        </NavLink>
+
+        <NavLink className="cart-link header-link" to="/checkout">
+          <img className="cart-icon" src={CartIcon} />
+          <div className="cart-quantity">{cartQuantity}</div>
+          <div className="cart-text">Cart</div>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
