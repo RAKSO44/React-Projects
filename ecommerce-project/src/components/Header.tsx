3ffc559515bbf77920386ade2d5a6e69a5c9a03d
RAKSO44 <oscarcruzaleguicastillo@gmail.com>
2025-11-03 15:39:42 -0500

    Convert money utils and Header to TypeScript

--- ecommerce-project/src/components/Header.tsx
+++ ecommerce-project/src/components/Header.tsx
@@ -6,8 +6,15 @@ import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import './Header.css';
 
+type HeaderProps = {
+  cart: {
+    productId: string;
+    quantity: number;
+    deliveryOptionId: string;
+  } [];
+}
 
-export function Header({ cart }) {
+export function Header({ cart } : HeaderProps) {
 
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
@@ -21,7 +28,7 @@ export function Header({ cart }) {
     cartQuantity += cartItem.quantity
   });
 
-  const updateSearchInput = (event) => {
+  const updateSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   };
 
